c1cfc488bfac34ee3c23d82653d7740f6aba0bb7
Yegorov <yegorov0725@yandex.ru>
2015-04-02 12:51:14 +0300

    add readme & some bugfix

---
+++ README.md
@@ -0,0 +1,22 @@
+# Web AWeather
+
+## Overview
+
+Simple parsing weather forecast write on Ruby on Rails.
+
+This application is deployed in Heroku - [aweather.herokuapp.com](http://aweather.herokuapp.com/)
+
+## Features 
+
+* Сomputation sunrise, sunset, moonphase for current date.
+
+
+## Interface
+
+![screen1](/screenshot1.png)
+
+## Third party libraries
+
+* [*Bootstrap*](http://getbootstrap.com/)
+* [*JQuery*](http://jquery.com/)
+* more utils find in Gemfile
--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -10,23 +10,20 @@ class WeatherController < ApplicationController
     @sunset = "17:00"
     @moon = "Full"
     
-    @date = Time.now.localtime("+02:00")
+    @date = Time.now.localtime("+03:00")
   end
   
   def get
     @day = "%02d" % @date.day
     @month = "%02d" % @date.month
     @year = @date.year.to_s
+	
     @moon = StaticData.get_moon(@date.year.to_i, @date.month.to_i, @date.day.to_i)
     
-    param_sun = StaticData.get_sun(@date.year.to_i, @date.month.to_i, @date.day.to_i, 48.333611, 38.0925, +2, 90.8333333333)
+    param_sun = StaticData.get_sun(@date.year.to_i, @date.month.to_i, @date.day.to_i, 48.333611, 38.0925, +3, 90.8333333333)
     @sunrise = param_sun[:sunrise]
     @sunset = param_sun[:sunset]
     
-    #test
-    #@temp = params[:temp]
-    #@moon = params[:moon]
-
     weather = Parser.parse
     @temp = weather[:temp]
     @bar = weather[:bar]
